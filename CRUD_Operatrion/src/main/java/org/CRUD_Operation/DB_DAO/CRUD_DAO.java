6570373503559973efba3d7a83fc94f5c0fcf088
Venkatesh <84002107+VenkatCodeOn@users.noreply.github.com>
2022-08-02 15:42:46 +0530

    Delete CRUD_Operatrion directory

--- CRUD_Operatrion/src/main/java/org/CRUD_Operation/DB_DAO/CRUD_DAO.java
+++
@@ -1,9 +0,0 @@
-package org.CRUD_Operation.DB_DAO;
-
-import org.CRUD_Operation.DB_Model.CRUD_Model;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CRUD_DAO extends CrudRepository<CRUD_Model, Integer> {
-	
-
-}
